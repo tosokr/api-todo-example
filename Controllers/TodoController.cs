ab0ad2136ecff2466a7f4e50872b1f237ce9113b
Aleksandar Stefanov <tosokr@gmail.com>
2019-09-18 19:33:34 +0200

    initial commit

---
+++ Controllers/TodoController.cs
@@ -0,0 +1,113 @@
+﻿#define Primary
+#if Primary
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Swashbuckle.AspNetCore.Annotations;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TodoApi.Models;
+
+#region TodoController
+namespace TodoApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TodoController : ControllerBase
+    {
+        private readonly TodoContext _context;
+        #endregion
+
+        public TodoController(TodoContext context)
+        {
+            _context = context;
+
+            if (_context.TodoItems.Count() == 0)
+            {
+                // Create a new TodoItem if collection is empty,
+                // which means you can't delete all TodoItems.
+                _context.TodoItems.Add(new TodoItem { Name = "Item1" });
+                _context.SaveChanges();
+            }
+        }
+
+        #region snippet_GetAll
+        // GET: api/Todo
+        [HttpGet]
+        [SwaggerOperation("GetTodoItems")]
+        public async Task<ActionResult<IEnumerable<TodoItem>>> GetTodoItems()
+        {
+            return await _context.TodoItems.ToListAsync();
+        }
+
+        #region snippet_GetByID
+        // GET: api/Todo/5
+        [HttpGet("{id}")]
+        [SwaggerOperation("GetTodoItem")]
+        public async Task<ActionResult<TodoItem>> GetTodoItem(long id)
+        {
+            var todoItem = await _context.TodoItems.FindAsync(id);
+
+            if (todoItem == null)
+            {
+                return NotFound();
+            }
+
+            return todoItem;
+        }
+        #endregion
+        #endregion
+
+        #region snippet_Create
+        // POST: api/Todo
+        [HttpPost]
+        [SwaggerOperation("PostTodoItem")]
+        public async Task<ActionResult<TodoItem>> PostTodoItem(TodoItem item)
+        {
+            _context.TodoItems.Add(item);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetTodoItem), new { id = item.Id }, item);
+        }
+        #endregion
+
+        #region snippet_Update
+        // PUT: api/Todo/5
+        [HttpPut("{id}")]
+        [SwaggerOperation("PutTodoItem")]
+        public async Task<IActionResult> PutTodoItem(long id, TodoItem item)
+        {
+            if (id != item.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(item).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+        #endregion
+
+        #region snippet_Delete
+        // DELETE: api/Todo/5
+        [HttpDelete("{id}")]
+        [SwaggerOperation("DeleteTodoItem")]
+        public async Task<IActionResult> DeleteTodoItem(long id)
+        {
+            var todoItem = await _context.TodoItems.FindAsync(id);
+
+            if (todoItem == null)
+            {
+                return NotFound();
+            }
+
+            _context.TodoItems.Remove(todoItem);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+        #endregion
+    }
+}
+#endif
---
+++ SwaggerExtension.cs
@@ -0,0 +1,36 @@
+﻿using Swashbuckle.AspNetCore.Swagger;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TodoApi
+{
+    public class SwaggerExtension
+    {
+        public class SwaggerOperationNameFilter : IOperationFilter
+        {
+            public void Apply(Operation operation, OperationFilterContext context)
+            {
+                operation.OperationId = context.MethodInfo.DeclaringType.GetCustomAttributes(true)
+                    .Union(context.MethodInfo.GetCustomAttributes(true))
+                    .OfType<SwaggerOperationAttribute>()
+                    .Select(a => a.OperationId)
+                    .FirstOrDefault();
+            }
+
+        }
+
+        [AttributeUsage(AttributeTargets.Method)]
+        public sealed class SwaggerOperationAttribute : Attribute
+        {
+            public SwaggerOperationAttribute(string operationId)
+            {
+                OperationId = operationId;
+            }
+
+            public string OperationId { get; }
+        }
+    }
+}
